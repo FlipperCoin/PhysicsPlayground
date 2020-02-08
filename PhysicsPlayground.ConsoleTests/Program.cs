e57db682916ca1970ae35f30f342c47ff7d64b78
FlipperCoin <itai10020@gmail.com>
2020-02-08 23:22:55 +0200

    Test MathNet's root finder for a polynomial

--- PhysicsPlayground.ConsoleTests/Program.cs
+++ PhysicsPlayground.ConsoleTests/Program.cs
@@ -1,10 +1,13 @@
-﻿using CenterSpace.NMath.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using CenterSpace.NMath.Analysis;
+using MathNet.Numerics;
+using PhysicsPlayground.Math;
+using Polynomial = PhysicsPlayground.Math.Polynomial;
 
 namespace PhysicsPlayground.ConsoleTests
 {
@@ -12,26 +15,10 @@ namespace PhysicsPlayground.ConsoleTests
     {
         static void Main(string[] args)
         {
-            var v_ax = new Polynomial(new DoubleVector("9.8"));
-            var v_vx = (v_ax.AntiDerivative() + new Polynomial(new DoubleVector("10")));
-            var v_x = (v_vx.AntiDerivative() + new Polynomial(new DoubleVector("-100")));
-
-            var u_ax = new Polynomial(new DoubleVector("9.7"));
-            var u_vx = (u_ax.AntiDerivative() + new Polynomial(new DoubleVector("10")));
-            var u_x = (u_vx.AntiDerivative() + new Polynomial(new DoubleVector("100")));
-
-            var deltaUxVx = v_x - u_x;
-            var rootFinder = new NewtonRaphsonRootFinder();
-
-            try
-            {
-                var meetingPoint = rootFinder.Find(deltaUxVx, deltaUxVx.Derivative(),
-                    new Interval(0, 100, Interval.Type.Closed));
-                Console.WriteLine(meetingPoint);
-            } catch(NMathException e) {
-                Console.WriteLine(e.Message);
-            }
-
+            Polynomial p = new Polynomial(36,0,-7,1);
+            Complex[] complexRoots = FindRoots.Polynomial(p.Coefficients);
+            var roots = complexRoots.Where(c => c.IsReal()).Distinct().Select(c => c.Real).ToList();
+            roots.ForEach(Console.WriteLine);
         }
     }
 }
